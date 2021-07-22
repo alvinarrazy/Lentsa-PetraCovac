cbdfa93c8e3e90e6e652243c0e4dd680a3099d86
Alvin Arrazy <51030862+alvinarrazy@users.noreply.github.com>
2021-07-23 00:27:46 +0700

    little tweaks on data serve

--- src/Pages/DesaStatsPage.js
+++ src/Pages/DesaStatsPage.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router';
 import {
 	getAllKecamatan,
@@ -9,7 +9,7 @@ import {
 } from '../redux/actions/CovidAction';
 import './Styles/Table.css'
 import Navbar from './Components/Navbar';
-import {API} from '../config';
+import { API } from '../config';
 import Footer from './Components/Footer';
 class StatsPage extends React.Component {
 	constructor(props) {
@@ -17,18 +17,20 @@ class StatsPage extends React.Component {
 
 		this.state = {
 			namaKecamatan: "",
-			desa: []
+			desa: [],
+			isLoaded: false
 		}
 	}
 
 	async componentWillMount() {
 		try {
-			
+
 			const idKecamatan = window.location.search.replace('?', '');
 			let resultDesa = await axios.get(`${API}/covid/get-desa-in-kecamatan/${idKecamatan}`)
 			this.setState({
 				desa: resultDesa.data.semua_desa,
-				namaKecamatan: resultDesa.data.semua_desa[0].nama_kecamatan
+				namaKecamatan: resultDesa.data.semua_desa[0].nama_kecamatan,
+				isLoaded: true
 			})
 		}
 		catch (error) {
@@ -39,44 +41,46 @@ class StatsPage extends React.Component {
 
 
 	render() {
-		const { desa,namaKecamatan } = this.state
+		const { desa, namaKecamatan, isLoaded } = this.state
 		return (
 			<>
-			<Navbar src={window.location.origin + "/images/lentsa.png"}/>
-				<h1>{namaKecamatan}</h1>
-				<table className="data-kecamatan" style={{tableLayout: 'fixed'}}>
-					<tr>
-						<th>Nama Desa</th>
-						<th>Suspek</th>
-						<th>Discharded</th>
-						<th>Meninggal</th>
-						<th>Konfirmasi Symptomatik</th>
-						<th>Konfirmasi Asymptomatik</th>
-						<th>Konfirmasi Sembuh</th>
-						<th>Konfirmasi Meninggal</th>
-						<th>Keterangan</th>
-					</tr>
-					<Fragment>
-						{
-							desa.map(satuDesa => {
-								return (
-									<tr>
-										<td>{satuDesa.nama_desa}</td>
-										<td className='number-data'>{satuDesa.suspek}</td>
-										<td className='number-data'>{satuDesa.discharded}</td>
-										<td className='number-data'>{satuDesa.meninggal}</td>
-										<td className='number-data'>{satuDesa.konfirmasi_symptomatik}</td>
-										<td className='number-data'>{satuDesa.konfirmasi_asymptomatik}</td>
-										<td className='number-data'>{satuDesa.konfirmasi_sembuh}</td>
-										<td className='number-data'>{satuDesa.konfirmasi_meninggal}</td>
-										<td>{satuDesa.keterangan}</td>
-									</tr>
-								)
-							})
-						}
-					</Fragment>
-				</table>
-				<Footer/>
+				<Navbar src={window.location.origin + "/images/lentsa.png"} />
+				{isLoaded ? <>
+					<h1>{namaKecamatan}</h1>
+					<table className="data-kecamatan" style={{ tableLayout: 'fixed' }}>
+						<tr>
+							<th>Nama Desa</th>
+							<th>Suspek</th>
+							<th>Discharded</th>
+							<th>Meninggal</th>
+							<th>Konfirmasi Symptomatik</th>
+							<th>Konfirmasi Asymptomatik</th>
+							<th>Konfirmasi Sembuh</th>
+							<th>Konfirmasi Meninggal</th>
+							<th>Keterangan</th>
+						</tr>
+						<Fragment>
+							{
+								desa.map(satuDesa => {
+									return (
+										<tr>
+											<td>{satuDesa.nama_desa}</td>
+											<td className='number-data'>{satuDesa.suspek}</td>
+											<td className='number-data'>{satuDesa.discharded}</td>
+											<td className='number-data'>{satuDesa.meninggal}</td>
+											<td className='number-data'>{satuDesa.konfirmasi_symptomatik}</td>
+											<td className='number-data'>{satuDesa.konfirmasi_asymptomatik}</td>
+											<td className='number-data'>{satuDesa.konfirmasi_sembuh}</td>
+											<td className='number-data'>{satuDesa.konfirmasi_meninggal}</td>
+											<td>{satuDesa.keterangan}</td>
+										</tr>
+									)
+								})
+							}
+						</Fragment>
+					</table>
+					<Footer />
+				</> : <></>}
 			</>
 		)
 
--- src/Pages/StatsPage.js
+++ src/Pages/StatsPage.js
@@ -1,14 +1,10 @@
 import React, { Fragment } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom';
-import {
-	getAllKecamatan,
-	getSumDataKecamatan
-} from '../redux/actions/CovidAction';
+import { Link } from 'react-router-dom';
 import './Styles/Table.css'
 import Navbar from './Components/Navbar';
-import {API} from '../config';
+import { API } from '../config';
 import Footer from './Components/Footer'
 
 class StatsPage extends React.Component {
@@ -17,7 +13,7 @@ class StatsPage extends React.Component {
 
 		this.state = {
 			kecamatan: [],
-			submitted: false
+			isLoaded: false
 		}
 	}
 
@@ -25,13 +21,16 @@ class StatsPage extends React.Component {
 		try {
 			let resultKecamatan = await axios.get(`${API}/covid/get-all-kecamatan`)
 			var resultSum
-			resultKecamatan.data.semua_kecamatan.map(async (kecamatan) =>{
+			Promise.all(resultKecamatan.data.semua_kecamatan.map(async (kecamatan) => {
 				resultSum = await axios.get(`${API}/covid/get-sum-data-kecamatan/${kecamatan._id}`)
 				console.log(resultSum)
 				this.setState({
 					kecamatan: [...this.state.kecamatan, resultSum.data]
 				})
-		   })
+			})).then(() => {
+				this.setState({ isLoaded: true });
+				console.log(this.state.isLoaded)
+			}) 
 		}
 		catch (error) {
 			console.log(error)
@@ -41,41 +40,43 @@ class StatsPage extends React.Component {
 
 
 	render() {
-		const { kecamatan } = this.state
+		const { kecamatan, isLoaded } = this.state
 		return (
 			<>
-			<Navbar/>
-				<table className="data-kecamatan">
-					<tr>
-						<th>Nama Kecamatan</th>
-						<th>Suspek</th>
-						<th>Discharded</th>
-						<th>Meninggal</th>
-						<th>Konfirmasi Symptomatik</th>
-						<th>Konfirmasi Asymptomatik</th>
-						<th>Konfirmasi Sembuh</th>
-						<th>Konfirmasi Meninggal</th>
-					</tr>
-					<Fragment>
-						{
-							kecamatan.map(satuKecamatan => {
-								return (
-									<tr>
-										<td><Link to={`/stats-data/data-desa/:?${satuKecamatan.id_kecamatan}`}>{satuKecamatan.nama_kecamatan}</Link></td>
-										<td>{satuKecamatan.suspek}</td>
-										<td>{satuKecamatan.discharded}</td>
-										<td>{satuKecamatan.meninggal}</td>
-										<td>{satuKecamatan.konfirmasi_symptomatik}</td>
-										<td>{satuKecamatan.konfirmasi_asymptomatik}</td>
-										<td>{satuKecamatan.konfirmasi_sembuh}</td>
-										<td>{satuKecamatan.konfirmasi_meninggal}</td>
-									</tr>
-								)
-							})
-						}
-					</Fragment>
-				</table>
-				<Footer/>
+				<Navbar />
+				{isLoaded ? <>
+					<table className="data-kecamatan">
+						<tr>
+							<th>Nama Kecamatan</th>
+							<th>Suspek</th>
+							<th>Discharded</th>
+							<th>Meninggal</th>
+							<th>Konfirmasi Symptomatik</th>
+							<th>Konfirmasi Asymptomatik</th>
+							<th>Konfirmasi Sembuh</th>
+							<th>Konfirmasi Meninggal</th>
+						</tr>
+						<Fragment>
+							{
+								kecamatan.map(satuKecamatan => {
+									return (
+										<tr>
+											<td><Link to={`/stats-data/data-desa/:?${satuKecamatan.id_kecamatan}`}>{satuKecamatan.nama_kecamatan}</Link></td>
+											<td>{satuKecamatan.suspek}</td>
+											<td>{satuKecamatan.discharded}</td>
+											<td>{satuKecamatan.meninggal}</td>
+											<td>{satuKecamatan.konfirmasi_symptomatik}</td>
+											<td>{satuKecamatan.konfirmasi_asymptomatik}</td>
+											<td>{satuKecamatan.konfirmasi_sembuh}</td>
+											<td>{satuKecamatan.konfirmasi_meninggal}</td>
+										</tr>
+									)
+								})
+							}
+						</Fragment>
+					</table>
+					<Footer />
+				</> : <></>}
 			</>
 		)
 
