0ea0e8990591a64e543fc2c4755fb6bf58f0cd29
Alvin Arrazy <51030862+alvinarrazy@users.noreply.github.com>
2021-07-24 15:08:30 +0700

    update navbar a little

--- src/Pages/Components/Navbar.js
+++ src/Pages/Components/Navbar.js
@@ -57,19 +57,19 @@ class Navbar extends Component {
                             }
                         </Fragment>
                         :
-                         <Fragment>
-                         {
-            MenuItems.map((item, index) => {
-                return (
-                    <li key={index}>
-                        <Link className={item.cName} to={item.url}>
-                            {item.title}
-                        </Link>
-                    </li>
-                )
-            })
-        }
-                         </Fragment>
+                        <Fragment>
+                            {
+                                MenuItems.map((item, index) => {
+                                    return (
+                                        <li key={index}>
+                                            <Link className={item.cName} to={item.url}>
+                                                {item.title}
+                                            </Link>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </Fragment>
                     }
                     <li key='4'>
                         <a className='nav-links-mobile' onClick={() => authHeader() ?
